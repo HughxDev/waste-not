e468de5ac8f4a57e39076ad4279ea66d00a5a828
Hugh Guiney <hugh@hughguiney.com>
2017-05-13 22:28:20 -0400

    Fuck Apple

--- src/platforms/android/platform_www/cordova_plugins.js
+++ src/platforms/android/platform_www/cordova_plugins.js
@@ -7,6 +7,22 @@ module.exports = [
         "clobbers": [
             "device"
         ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification",
+        "file": "plugins/cordova-plugin-dialogs/www/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
+    },
+    {
+        "id": "cordova-plugin-dialogs.notification_android",
+        "file": "plugins/cordova-plugin-dialogs/www/android/notification.js",
+        "pluginId": "cordova-plugin-dialogs",
+        "merges": [
+            "navigator.notification"
+        ]
     }
 ];
 module.exports.metadata = 
@@ -14,7 +30,8 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.3.2",
     "cordova-plugin-device": "1.1.6",
-    "cordova-plugin-console": "1.0.7"
+    "cordova-plugin-console": "1.0.7",
+    "cordova-plugin-dialogs": "1.3.3"
 };
 // BOTTOM OF METADATA
 });
